727d96c9339bd51266574e5a82e3a82fe6b1eeb9
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-02-19 11:40:59 -0300

    04.06_Inserindo_Recuperando_Mapa

--- mapas/mapa.py
+++ mapas/mapa.py
@@ -1,7 +1,7 @@
 from listas import lista_ligada
 from mapas import associacao
 
-# 0: ("par", 10) ("primos", 3)
+# 0: ("primos", 3) ("par", 20)
 # 1: 15, 25, 35,
 # 2: 16, 26, 36
 # 3 (150:200):
@@ -35,3 +35,23 @@ class Mapa():
                 categoria.remover_elemento(associacao)
                 return True
         return False
+
+    def adicionar(self, chave, valor):
+        if self.contem_chave(chave):
+            self.remover(chave)
+        numero_espalhamento = self.gerar_numero_espalhamento(chave)
+        categoria = self.__elementos.recuperar_elemento_no(numero_espalhamento)
+        categoria.inserir(associacao.Associacao(chave, valor))
+
+    def recuperar(self, chave):
+        numero_espalhamento = self.gerar_numero_espalhamento(chave)
+        categoria = self.__elementos.recuperar_elemento_no(numero_espalhamento)
+        for i in range(categoria.tamanho):
+            associacao = categoria.recuperar_elemento_no(i)
+            if associacao.chave == chave:
+                return associacao.valor
+        return False
+
+    def __str__(self):
+        temp = self.__elementos.__str__()
+        return temp
