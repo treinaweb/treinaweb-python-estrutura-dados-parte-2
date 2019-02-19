405fd88f775480b4cb63fa0b170c420dd8fcc4ff
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-02-19 11:30:38 -0300

    04.05_Pesquisando_Removendo_Chaves

--- mapas/mapa.py
+++ mapas/mapa.py
@@ -1,6 +1,7 @@
 from listas import lista_ligada
+from mapas import associacao
 
-# 0: ("par", 10)
+# 0: ("par", 10) ("primos", 3)
 # 1: 15, 25, 35,
 # 2: 16, 26, 36
 # 3 (150:200):
@@ -13,5 +14,24 @@ class Mapa():
         for i in range(self.__numero_categorias):
             self.__elementos.inserir(lista_ligada.ListaLigada())
 
-    def gerar_numero_espalhemnto(self, chave):
+    def gerar_numero_espalhamento(self, chave):
         return hash(chave) % self.__numero_categorias
+
+    def contem_chave(self, chave):
+        numero_espalhamento = self.gerar_numero_espalhamento(chave)
+        categoria = self.__elementos.recuperar_elemento_no(numero_espalhamento)
+        for i in range(categoria.tamanho):
+            associacao = categoria.recuperar_elemento_no(i)
+            if associacao.chave == chave:
+                return True
+        return False
+
+    def remover(self, chave):
+        numero_espalhamento = self.gerar_numero_espalhamento(chave)
+        categoria = self.__elementos.recuperar_elemento_no(numero_espalhamento)
+        for i in range(categoria.tamanho):
+            associacao = categoria.recuperar_elemento_no(i)
+            if associacao.chave == chave:
+                categoria.remover_elemento(associacao)
+                return True
+        return False
